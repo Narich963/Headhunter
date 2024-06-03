182e19757980a458be10d474cb552bc91ea658ea
Nariman Biiazbekov <narbiiaz@gmail.com>
2024-06-03 23:01:52 +0600

    Создан личный кабинет с редактированием через модальное окно

---
+++ Headhunter/Controllers/EmployeeController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Headhunter.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Headhunter.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly Context _context;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public EmployeeController(Context context, UserManager<User> manager, SignInManager<User> signInManager)
+        {
+            _context = context;
+            _userManager = manager;
+            _signInManager = signInManager;
+        }
+
+        // GET: Employee
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: Employee/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Employee/Edit/5
+        public async Task<IActionResult> Edit(int? userId)
+        {
+            if (userId == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return PartialView("_EditPartial", user);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    User oldUser = await _context.Users.FirstOrDefaultAsync(o => o.Id == id);
+                    oldUser.Name = user.Name;
+                    oldUser.PhoneNumber = user.PhoneNumber;
+                    oldUser.Email = user.Email;
+                    oldUser.UserName = user.UserName;
+                    oldUser.Avatar = user.Avatar;
+                    oldUser.NormalizedUserName = user.UserName.ToUpper();
+                    oldUser.NormalizedEmail = user.Email.ToUpper();
+                    _context.Update(oldUser);
+                    await _context.SaveChangesAsync();
+                    await _signInManager.SignOutAsync();
+                    return RedirectToAction("Login", "Account");
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            return View(user);
+        }
+
+        // GET: Employee/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
